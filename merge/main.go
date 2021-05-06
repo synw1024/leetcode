b9cc5f54fdc5a534a4a31d07b2208ec8e583219d
caoweijun <caoweijun@xiao100.com>
2021-05-06 18:09:27 +0800

    .

---
+++ merge/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	res := []int{1, 2, 3, 0, 0, 0}
+	merge(res, 3, []int{2, 5, 6}, 3)
+	fmt.Println(res)
+}
+
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	copy(nums1[m:], nums2)
+	sort.Ints(nums1)
+}
