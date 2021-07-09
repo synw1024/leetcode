1d655b748dab966225f477ab8ce102778cd0f045
caoweijun <caoweijun@xiao100.com>
2021-07-09 16:36:13 +0800

    5

--- 5/main.go
+++ 5/main.go
@@ -2,63 +2,53 @@ package main
 
 import (
 	"fmt"
-	"strconv"
-	"strings"
 )
 
 func main() {
-	res := longestPalindrome("cbbd")
+	res := longestPalindrome("babad")
 	fmt.Println(res)
 }
 
 func longestPalindrome(s string) string {
 	l := len(s)
 
-	dp := make([][3]string, l)
-	dp[0][0] = "0,0"
-	dp[0][1] = "0,1"
-	dp[0][2] = "0,-1"
-
-	for i := 1; i < l; i++ {
-		dp[i][0] = strconv.Itoa(i) + "," + strconv.Itoa(i)
-		dp[i][1] = max(s, strconv.Itoa(i)+","+strconv.Itoa(i+1), palindromeIndex(dp[i-1][1], i, s))
-		dp[i][2] = max(s, dp[i-1][1], dp[i-1][2])
+	dp := make([][]int, l)
+	for i := 0; i < l; i++ {
+		dp[i] = make([]int, l)
 	}
 
-	indexes := strings.Split(max(s, dp[l-1][1], dp[l-1][2]), ",")
-	start := unsafeToNum(indexes[0])
-	end := unsafeToNum(indexes[1])
-	return s[start:end]
-}
-
-func palindromeIndex(indexesString string, index int, source string) string {
-	indexes := strings.Split(indexesString, ",")
-	start := unsafeToNum(indexes[0])
-
-	ss := source[start : index+1]
-	if !isPalindrome(ss) {
-		return indexesString
-	}
-	return strconv.Itoa(start) + "," + strconv.Itoa(index+1)
-}
-
-func max(s, s1, s2 string) string {
-	indexes1 := strings.Split(s1, ",")
-	indexes2 := strings.Split(s2, ",")
-	start1 := unsafeToNum(indexes1[0])
-	start2 := unsafeToNum(indexes2[0])
-	end1 := unsafeToNum(indexes1[1])
-	end2 := unsafeToNum(indexes2[1])
-
-	if end1-start1 > end2-start2 {
-		return strconv.Itoa(start1) + "," + strconv.Itoa(end1)
-	} else if end1-start1 == end2-start2 {
-		if start1 < start2 {
-			return strconv.Itoa(start1) + "," + strconv.Itoa(end1)
+	start := 0
+	length := 1
+	for i := 0; i < l; i++ {
+		j := i
+		k := i
+		for true {
+			if j < 0 || k >= l || !isPalindrome(s[j:k+1]) {
+				break
+			}
+			if k-j+1 > length {
+				start = j
+				length = k - j + 1
+			}
+			j--
+			k++
 		}
-		return strconv.Itoa(start2) + "," + strconv.Itoa(end2)
+		j = i
+		k = i + 1
+		for true {
+			if j < 0 || k >= l || !isPalindrome(s[j:k+1]) {
+				break
+			}
+			if k-j+1 > length {
+				start = j
+				length = k - j + 1
+			}
+			j--
+			k++
+		}
 	}
-	return strconv.Itoa(start2) + "," + strconv.Itoa(end2)
+
+	return s[start : start+length]
 }
 
 func isPalindrome(s string) bool {
@@ -72,9 +62,4 @@ func isPalindrome(s string) bool {
 		j--
 	}
 	return true
-}
-
-func unsafeToNum(s string) int {
-	n, _ := strconv.Atoi(s)
-	return n
 }
