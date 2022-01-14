0ec659141f3cfd3a01e8f9b3af7a788a8958a550
Synw <synw1024@yeah.net>
2022-01-14 15:38:36 +0800

    .

--- 5/main.go
+++ 5/main.go
@@ -12,18 +12,13 @@ func main() {
 func longestPalindrome(s string) string {
 	l := len(s)
 
-	dp := make([][]int, l)
-	for i := 0; i < l; i++ {
-		dp[i] = make([]int, l)
-	}
-
 	start := 0
 	length := 1
 	for i := 0; i < l; i++ {
 		j := i
 		k := i
 		for true {
-			if j < 0 || k >= l || !isPalindrome(s[j:k+1]) {
+			if j < 0 || k >= l || s[j] != s[k] {
 				break
 			}
 			if k-j+1 > length {
@@ -36,7 +31,7 @@ func longestPalindrome(s string) string {
 		j = i
 		k = i + 1
 		for true {
-			if j < 0 || k >= l || !isPalindrome(s[j:k+1]) {
+			if j < 0 || k >= l || s[j] != s[k] {
 				break
 			}
 			if k-j+1 > length {
@@ -49,17 +44,4 @@ func longestPalindrome(s string) string {
 	}
 
 	return s[start : start+length]
-}
-
-func isPalindrome(s string) bool {
-	l := len(s)
-
-	for i, j := 0, l-1; i < j; {
-		if s[i] != s[j] {
-			return false
-		}
-		i++
-		j--
-	}
-	return true
 }
