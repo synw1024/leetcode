f5c869c79dc3cc81206daf10fc64e1ddc2eda0df
Synw <synw1024@yeah.net>
2022-01-19 12:01:33 +0800

    .

--- 91/main.go
+++ 91/main.go
@@ -3,29 +3,33 @@ package main
 import "fmt"
 
 func main() {
-	res := numDecodings("27")
+	res := numDecodings("226")
 	fmt.Println(res)
 }
 
 // dp[i] = dp[i-1] + dp[i-2]
 func numDecodings(s string) int {
 	l := len(s)
-	dp := make([]int, l+1)
+	dp := make([]int, l)
 	if s[0] != '0' {
 		dp[0] = 1
+	} else {
+		return 0
 	}
-	for i := 1; i <= l; i++ {
-		if s[i-1] != '0' {
+	for i := 1; i < l; i++ {
+		if s[i] != '0' {
 			dp[i] += dp[i-1]
 		}
-		if i-2 >= 0 {
-			n := s[i-2] - '0'
-			nn := s[i-1] - '0'
-			if n == 1 || (n == 2 && nn < 7) {
+
+		n := s[i-1] - '0'
+		nn := s[i] - '0'
+		if n == 1 || (n == 2 && nn < 7) {
+			if i-2 >= 0 {
 				dp[i] += dp[i-2]
+			} else {
+				dp[i]++
 			}
 		}
-
 	}
-	return dp[l]
+	return dp[l-1]
 }
