09f74c95bb3ed646951249b4b64a95115dcbfee7
Synw <synw1024@yeah.net>
2022-09-18 13:34:02 +0800

    Longest Substring Without Repeating Characters

--- 3.js
+++ 3.js
@@ -3,12 +3,25 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function (s) {
-  let max, start, end = 0
-  const letterPositionMap = {}
+  let max = 0, newMax = 0, start = 0
+  const map = {}
   for (let i = 0; i < s.length; i++) {
     const letter = s[i]
-    if (!letterPositionMap[letter]) {
-
+    if (map[letter] === undefined || map[letter] < start) {
+      map[letter] = i
+      newMax++
+    } else {
+      max = newMax > max ? newMax : max
+      newMax = i - map[letter]
+      start = map[letter] + 1
+      map[letter] = i
     }
   }
+  return newMax > max ? newMax : max
 };
+
+console.log(lengthOfLongestSubstring("abcabcbb"))
+console.log(lengthOfLongestSubstring("bbbbb"))
+console.log(lengthOfLongestSubstring("pwwkew"))
+console.log(lengthOfLongestSubstring("abba"))
+console.log(lengthOfLongestSubstring("tmmzuxt"))
