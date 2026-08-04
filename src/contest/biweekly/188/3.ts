da62be269ee4011245cf74b6e24e08f139ddc708
熵 <synw1024@yeah.net>
2026-08-04 16:35:11 +0800

    feat: biweekly 188-3

--- src/contest/biweekly/188/3.ts
+++ src/contest/biweekly/188/3.ts
@@ -1,27 +1,19 @@
 function minInitialStrength(monsters: number[], boosts: number[][]): number {
-  const diff: number[] = []
-  for (let i = 0; i < boosts.length; i++) {
-    const [start, end, val] = boosts[i]
-    diff[start] = diff[start] ? diff[start] + val : val
-    diff[end+1] = diff[end+1] ? diff[end+1] - val : -val
-  }
-  const bonus: number[] = []
-  bonus[0] = diff[0] || 0
-  for (let i = 1; i < monsters.length; i++) {
-    bonus[i] = bonus[i - 1] + (diff[i] || 0)
+  const diff: number[] = Array(monsters.length).fill(0)
+  for (const [start, end, val] of boosts) {
+    diff[start - 1] -= val
+    diff[end] += val
   }
 
-  let sum = 0
+  let sum = 0, bonus = 0
   for (let i = monsters.length - 1; i >= 0; i--) {
     const m = monsters[i]
+    bonus += diff[i]
 
-    if (sum > 0 && i < monsters.length - 1) {
+    if (sum > 0) {
       sum += m
-    }
-
-    const diff = m - (bonus[i] || 0) - sum
-    if (diff > 0) {
-      sum += diff
+    } else {
+      sum += Math.max(m - bonus, 0)
     }
 
   }
