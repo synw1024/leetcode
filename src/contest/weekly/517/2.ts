d93c731f572ca87c468ef45eeb6316a317ca69ea
熵 <synw1024@yeah.net>
2026-09-05 16:59:38 +0800

    feat: 517-2

--- src/contest/weekly/517/2.ts
+++ src/contest/weekly/517/2.ts
@@ -1,26 +1,25 @@
 function sumDecoded(nums: number[]): number {
-  return nums.reduce((sum, cur) => {
+  const mod = 1000000007n
+  const res = nums.reduce((sum, cur) => {
     const width = cur % 10
     const d = Math.floor(cur / 10)
     const s = d.toString()
-    const x = parseInt(s.slice(0, width))
-    const y = parseInt(s.slice(width))
+    let x = BigInt(parseInt(s.slice(0, width)))
+    let y = parseInt(s.slice(width))
 
-    let n = y, times = 1, remain = 0
-    while (Math.pow(x, n) > Math.pow(Number.MAX_SAFE_INTEGER, 0.5)) {
-      if (n % 2 > 0) remain += times
-      n = Math.floor(n / 2)
-      times *= 2
+    let res = 1n
+    while (y > 0) {
+      if (y & 1) {
+        res = (res * x) % mod
+      }
+      x = x * x % mod
+      y = y >> 1
     }
-
-    let c = Math.pow(x, remain) % 1000000007
-    for (let i = 0; i < times; i++) {
-      c = (c * Math.pow(x, n)) % 1000000007
-    }
-
-    return (sum + c) % 1000000007
-  }, 0)
+    return (sum + res) % mod
+  }, 0n)
+  return Number(res)
 };
 
 // console.log(sumDecoded([59412]))
-console.log(sumDecoded([379723]))
+// console.log(sumDecoded([379723]))
+console.log(sumDecoded([17752335231789]))
