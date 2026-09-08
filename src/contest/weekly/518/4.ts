9701b151dbaea752c55329f4f33c45df63650a36
熵 <synw1024@yeah.net>
2026-09-08 09:09:41 +0800

    feat: adjust "dirs"

--- src/contest/weekly/518/4.ts
+++ src/contest/weekly/518/4.ts
@@ -1,16 +1,17 @@
 function minCost(grid: number[][], k: number): number {
   const m = grid.length
   const n = grid[0].length
+  const max = 1000 * m * n
 
   const cache: { [key: string]: number } = {}
-  const dirs = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+  const dirs = [[0, 1], [1, 0], [0, -1], [-1, 0]]
   function recurse(i: number, j: number, direction: number, turn: number): number {
     if (i === m - 1 && j === n - 1) return grid[i][j]
 
     const key = i + ',' + j + ',' + direction + ',' + turn
     if (cache[key] !== undefined) return cache[key]
 
-    let min = Number.MAX_SAFE_INTEGER
+    let min = max
     for (let d = 0; d < dirs.length; d++) {
       const [x, y] = [i + dirs[d][0], j + dirs[d][1]]
       if (x < 0 || x >= m || y < 0 || y >= n || (direction !== -1 && (d + 2) % 4 === direction)) continue
@@ -19,11 +20,14 @@ function minCost(grid: number[][], k: number): number {
       if (t < 0) continue
 
       min = Math.min(min, recurse(x, y, d, t) + grid[i][j])
+      if (!min) break
     }
 
     return cache[key] = min
   }
 
   const res = recurse(0, 0, -1, k)
-  return res >= Number.MAX_SAFE_INTEGER ? -1 : res
+  return res >= max ? -1 : res
 };
+
+console.log(minCost([[0,0,0],[0,1,0],[0,0,0]], 2))
